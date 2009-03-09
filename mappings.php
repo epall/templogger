84c47fbc095e68c57f7c3272c35321fc85703bab
Eric Allen <ericpallen@gmail.com>
2009-03-09 18:23:10 -0400

    New post script for handling data from a Web Energy Logger

--- mappings.php
+++ mappings.php
@@ -1,46 +1,11 @@
 <?php
-// 080118 commented out unplugged sensors 8e 92 bc 3a 03 99
 $mappings = array();
-//
-//
-//      FO is built ino the humidity module but is not identical so sw does not poll it
-
-$mappings[] = array("67 hot Rt", "6700000105615828", "red");
-//$mappings[] = array("8E hot Left", "8E000000FC9FF328", "purple");      // activated 071127  ~ 3:00 am
-//$mappings[] = array("92 cold Left", "9200000109D4E428", "blue");  // activated 071127 20:24
-//$mappings[] = array("BC cold Rt", "BC0000010507A728", "lime");      // activated 071127 20:24
-
-$mappings[] = array("F8 MB mid", "F8000001096CA628", "blue");
-//$mappings[] = array("3A MB bot", "3A000001095EF328", "violet");
-$mappings[] = array("36 PB mid", "3600000109546328", "green");
-
-
-// foo
-
-
-//$mappings[] = array("03 into MB", "0300000109DBDA28", "brown");
-//$mappings[] = array("BB outof MB", "BB00000109E0E228", "pink");
-//$mappings[] = array("3F out of PB", "3F0000010A0D0C28", "purple");
-
-$mappings[] = array("E7 into HV", "E700000109D91128", "orange");
-$mappings[] = array("B6 outof HV", "B600000109729A28", "cyan");
-
-
-$mappings[] = array("E5 humidity", "E5000000A93A1426", "blue");
-$mappings[] = array("29 Attic", "2900000089A13228", "firebrick");
-$mappings[] = array("18 Deck", "18000000FCA1E628", "orange");
-$mappings[] = array("2D Bath", "2D000000FBF82B28", "black");
-//$mappings[] = array("4C Cellar", "4C000000FBE5E928", "purple");
-//below is for the 4x10 Alten 110GS collextor);
-
-// below added Jan 18 2007
-// new as of jan 21 2007
-$mappings[] = array("A0 HWH flue", "A000000109912D28", "purple");
-//$mappings[] = array("CD supply", "CD00000109B0AB28", "lime");
-$mappings[] = array("F2 into GHWH", "F200000104E86228", "yellowgreen");
-//$mappings[] = array("A1 GHW out", "A1000001097A7A28", "red");
-$mappings[] = array("D1 2ft", "D100000109525A28", "blue");
-$mappings[] = array("C0 gwh exit", "C0000001096D4228", "red");
-//$mappings[] = array("9D 12ft", "9D00000109505028", "black");
-//$mappings[] = array("CF", "CF00000109B5C528", "cyan");
+$mappings[] = array("T1K", "T1K", "red");
+$mappings[] = array("T2H", "T2H", "green");
+$mappings[] = array("T3", "T3", "blue");
+$mappings[] = array("T4", "T4", "pink");
+$mappings[] = array("T5", "T5", "purple");
+$mappings[] = array("T6", "T6", "orange");
+$mappings[] = array("T7", "T7", "magenta");
+$mappings[] = array("T8", "T8", "cyan");
 ?>
---
+++ welpost.php
@@ -0,0 +1,62 @@
+<?php
+// forward data to Phil's welserver.com
+$testing = @$_GET['test'] == 1; // check for testing mode
+if(!$testing){   // IF not testing, forward GET parameters to OCH and send the response back to the WEL
+  $ochRequest = curl_init();
+  curl_setopt($ochRequest, CURLOPT_URL, "http://www.welserver.com/cgi-bin/WEL_post.cgi?".$_SERVER["QUERY_STRING"]);
+  curl_setopt($ochRequest, CURLOPT_HEADER, false);
+  $header = array("Connection" => "Close", "host" => "www.welserver.com");
+  curl_setopt($ochRequest, CURLOPT_HTTPHEADER, $header);
+  curl_exec($ochRequest);
+  curl_close($ochRequest);
+} else { // testing mode
+  $ochRequest = curl_init();
+  curl_setopt($ochRequest, CURLOPT_URL, "http://sundialtelemetrics.com/templogger/test.php?".$_SERVER["QUERY_STRING"]);
+  curl_setopt($ochRequest, CURLOPT_HEADER, false);
+  $header = array("Connection" => "Close", "host" => "sundialtelemetrics.com");
+  curl_setopt($ochRequest, CURLOPT_HTTPHEADER, $header);
+  curl_exec($ochRequest);
+  curl_close($ochRequest);
+}
+?>
+
+<?php
+function sensor($sensorName){
+  switch($sensorName){
+    case "Uu":
+    return false;
+    case "Ii":
+    return false;
+    case "Vv":
+    return false;
+    case "Ee":
+    return false;
+    case "Pp":
+    return false;
+    case "test":
+    return false;
+    case "Date":
+    return false;
+    case "Time":
+    return false;
+    default:
+    return true;
+  }
+}
+$values = array_filter(array_keys($_GET), sensor);
+
+$handle = fopen("tinidata.txt", "a");
+fwrite($handle, 'DATE '.$_GET['Date'].' '.$_GET['Time']."\n");
+foreach($values as $key){
+  fwrite($handle, $key);
+  fwrite($handle, ',');
+
+  // divide by 1000 due to WEL stupidity
+  if($key[0] == 'T')
+    fwrite($handle, $_GET[$key]/1000);
+  else
+    fwrite($handle, $_GET[$key]);
+  fwrite($handle, "\n");
+}
+fclose($handle);
+?>
